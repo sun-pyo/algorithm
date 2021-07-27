9718dff257df2ff5e99c857f25cea54b11f83093
sun-pyo <sunpyu97@gmail.com>
2021-07-27 21:43:42 +0900

    2021-07-27 프로그래머스 60060 가사 검색

---
+++ programmers/60060.cpp
@@ -0,0 +1,60 @@
+0#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Trie{
+private:
+    Trie* child[26];
+    int cnt;
+    
+public:
+    Trie() : child(), cnt(0){}
+    
+    void Insert(string str){
+        Trie* curr = this;
+        
+        for(char ch : str){
+            curr->cnt++;
+            
+            if(curr->child[ch-'a'] == nullptr) curr->child[ch-'a'] = new Trie();
+            curr = curr->child[ch-'a'];
+        }
+        curr->cnt++;
+    }
+    
+    int Search(string str){
+        Trie* curr = this;
+        
+        for(char ch : str){
+            if(ch == '?') return curr->cnt;
+            
+            curr = curr->child[ch-'a'];
+            if(curr == nullptr) return 0;
+        }
+        return 0;
+    }
+};
+
+Trie trie[10000];
+Trie reverse_trie[10000];
+
+
+vector<int> solution(vector<string> words, vector<string> queries) {
+    vector<int> answer;
+    for(string s : words){
+        trie[s.size()-1].Insert(s);
+        reverse(s.begin(), s.end());
+        reverse_trie[s.size()-1].Insert(s);
+    }
+    
+    for(string q : queries){
+        if(q[0] != '?') answer.push_back(trie[q.size()-1].Search(q));
+        else{
+            reverse(q.begin(), q.end());
+            answer.push_back(reverse_trie[q.size()-1].Search(q));
+        }
+    }
+    return answer;
+}
