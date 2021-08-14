57a0e6279cd76b15f9811fc86c2e4ce9a066323a
sun-pyo <sunpyu97@gmail.com>
2021-08-14 12:29:17 +0900

    2021-08-14 프로그래머스 12914 멀리 뛰기

---
+++ programmers/12914.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long DP[2001] = {0,};
+
+long long solution(int n) {
+    long long answer = 0;
+    DP[0] = 1;
+    DP[1] = 1;
+    for(int i=2;i<=n;i++){
+        DP[i] = (DP[i-1] + DP[i-2])%1234567;
+    }
+    answer = DP[n];
+    return answer;
+}
